75ae2ffd6df9eb807c372e51e6a244d2d58b4f98
ValentinaRuiz00 <valentinaruiztapias1234@gmail.com>
2024-08-16 15:31:21 -0500

    Se agrega el componente spinner

---
+++ WebApp/wwwroot/js/spinner.js
@@ -0,0 +1,7 @@
+﻿function showSpinner() {
+    document.getElementById('spinner').style.display = "block";
+}
+
+function hideSpinner() {
+    document.getElementById('spinner').style.display = "none";
+}
