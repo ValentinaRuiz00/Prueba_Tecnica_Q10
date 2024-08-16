b841836d5f5edce947174d1dec126b9bc6c84dd8
ValentinaRuiz00 <valentinaruiztapias1234@gmail.com>
2024-08-15 22:52:06 -0500

    Se agrega modelo de empleado

---
+++ Model/Entities/EmployeerEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Model.Entities
+{
+    public class EmployeerEntity
+    {
+        //Modelo de la base de datos Employeers
+        public int IdEmployeers { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Office { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
